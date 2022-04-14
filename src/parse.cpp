177e2a34c2c301d492536f794d32fe70c9d9981b
Paul D'Angio <pcdangio@gmail.com>
2022-04-13 22:14:24 -0400

    parse: updated methodology and added base types

--- src/parse.cpp
+++ src/parse.cpp
@@ -1,53 +1,118 @@
 #include "parse.hpp"
 
-double nmea::parse::utc(const std::string& utc_string)
+// BASE
+void nmea::parse::set_uint8(nmea::field<uint8_t>& field, const nmea::sentence& sentence, uint8_t field_index)
 {
-    // FORMAT: hhmmss.ss...
+    // Get field string.
+    std::string field_string = sentence.get_field(field_index);
 
-    // Parse hours.
-    double utc = std::stod(utc_string.substr(0,2)) * 3600.0;
-
-    // Parse minutes.
-    utc += std::stod(utc_string.substr(2,2)) * 60.0;
-
-    // Parse seconds.
-    utc += std::stod(utc_string.substr(4));
-
-    return utc;
-}
-double nmea::parse::latitude(const std::string& latitude_string, const std::string& direction_string)
-{
-    // FORMAT: ddmm.mm...
-
-    // Parse degrees.
-    double latitude = std::stod(latitude_string.substr(0,2));
-
-    // Parse minutes and convert to decimal degrees.
-    latitude += std::stod(latitude_string.substr(2)) / 60.0;
-
-    // Get latitude direction.
-    if(direction_string == "S")
+    // Check if field exists.
+    if(!field_string.empty())
     {
-        latitude *= -1.0;
+        // Set field with enum value.
+        field.set(std::stoul(field_string));
     }
-
-    return latitude;
 }
-double nmea::parse::longitude(const std::string& longitude_string, const std::string& direction_string)
+void nmea::parse::set_uint16(nmea::field<uint16_t>& field, const nmea::sentence& sentence, uint8_t field_index)
 {
-    // FORMAT: dddmm.mm...
+    // Get field string.
+    std::string field_string = sentence.get_field(field_index);
 
-    // Parse degrees.
-    double longitude = std::stod(longitude_string.substr(0,3));
-
-    // Parse minutes and convert to decimal degrees.
-    longitude += std::stod(longitude_string.substr(3)) / 60.0;
-
-    // Get longitude direction.
-    if(direction_string == "W")
+    // Check if field exists.
+    if(!field_string.empty())
     {
-        longitude *= -1.0;
+        // Set field with enum value.
+        field.set(std::stoul(field_string));
     }
+}
+void nmea::parse::set_float(nmea::field<float>& field, const nmea::sentence& sentence, uint8_t field_index)
+{
+    // Get field string.
+    std::string field_string = sentence.get_field(field_index);
 
-    return longitude;
+    // Check if field exists.
+    if(!field_string.empty())
+    {
+        // Set field with enum value.
+        field.set(std::stof(field_string));
+    }
+}
+
+// EXTENDED
+void nmea::parse::set_utc(nmea::field<double>& field, const nmea::sentence& sentence, uint8_t field_index)
+{
+    // Get field string.
+    std::string field_string = sentence.get_field(field_index);
+
+    // Check that field exists.
+    if(!field_string.empty())
+    {
+        // FORMAT: hhmmss.ss...
+
+        // Parse hours.
+        double utc = std::stod(field_string.substr(0,2)) * 3600.0;
+
+        // Parse minutes.
+        utc += std::stod(field_string.substr(2,2)) * 60.0;
+
+        // Parse seconds.
+        utc += std::stod(field_string.substr(4));
+
+        // Set field.
+        field.set(utc);
+    }
+}
+void nmea::parse::set_latitude(nmea::field<double>& field, const nmea::sentence& sentence, uint8_t field_index)
+{
+    // Get field strings.
+    std::string latitude_string = sentence.get_field(field_index);
+    std::string direction_string = sentence.get_field(field_index + 1);
+
+    // Check that both fields exist.
+    if(!latitude_string.empty() && !direction_string.empty())
+    {
+        // FORMAT: ddmm.mm...
+
+        // Parse degrees.
+        double latitude = std::stod(latitude_string.substr(0,2));
+
+        // Parse minutes and convert to decimal degrees.
+        latitude += std::stod(latitude_string.substr(2)) / 60.0;
+
+        // Get latitude direction.
+        if(direction_string == "S")
+        {
+            latitude *= -1.0;
+        }
+
+        // Set field.
+        field.set(latitude);
+    }
+}
+void nmea::parse::set_longitude(nmea::field<double>& field, const nmea::sentence& sentence, uint8_t field_index)
+{
+    // Get field strings.
+    std::string longitude_string = sentence.get_field(field_index);
+    std::string direction_string = sentence.get_field(field_index + 1);
+
+    // Check that both fields exist.
+    if(!longitude_string.empty() && !direction_string.empty())
+    {
+        // FORMAT: dddmm.mm...
+
+        // Parse degrees.
+        double longitude = std::stod(longitude_string.substr(0,3));
+
+        // Parse minutes and convert to decimal degrees.
+        longitude += std::stod(longitude_string.substr(2)) / 60.0;
+
+        // Get longitude direction.
+        if(direction_string == "W")
+        {
+            longitude *= -1.0;
+        }
+
+        // Set field.
+        field.set(longitude);
+    }
 }
--- src/parse.hpp
+++ src/parse.hpp
@@ -3,6 +3,9 @@
 #ifndef NMEA___PARSE_H
 #define NMEA___PARSE_H
 
+#include <nmea/field.hpp>
+#include <nmea/sentence.hpp>
+
 #include <string>
 
 namespace nmea {
@@ -10,20 +13,57 @@ namespace nmea {
 /// \brief Contains all code for parsing common NMEA field strings.
 namespace parse {
 
-/// \brief Parses the UTC time of day from an NMEA UTC field.
-/// \param utc_string The UTC field as a string.
-/// \returns The UTC as a decimal, in seconds.
-double utc(const std::string& utc_string);
-/// \brief Parses the latitude from NMEA latitude/direction fields.
-/// \param latitude_string The latitude field as a string.
-/// \param direction_string The latitude direction field as a string.
-/// \returns The latitude as a decimal, in degrees (N = +, S = -).
-double latitude(const std::string& latitude_string, const std::string& direction_string);
-/// \brief Parses the longitude from NMEA longitude/direction fields.
-/// \param longitude_string The longitude field as a string.
-/// \param direction_string The longitude direction field as a string.
-/// \returns The longitude as a decimal, in degrees (E = +, W = -).
-double longitude(const std::string& longitude_string, const std::string& direction_string);
+// BASE
+/// \brief Parses a UINT8 field from an NMEA sentence.
+/// \param field The NMEA field to parse the value into.
+/// \param sentence The NMEA sentence to read the value from.
+/// \param field_index The index of the field in the NMEA sentence.
+void set_uint8(nmea::field<uint8_t>& field, const nmea::sentence& sentence, uint8_t field_index);
+/// \brief Parses a UINT16 field from an NMEA sentence.
+/// \param field The NMEA field to parse the value into.
+/// \param sentence The NMEA sentence to read the value from.
+/// \param field_index The index of the field in the NMEA sentence.
+void set_uint16(nmea::field<uint16_t>& field, const nmea::sentence& sentence, uint8_t field_index);
+/// \brief Parses a float field from an NMEA sentence.
+/// \param field The NMEA field to parse the value into.
+/// \param sentence The NMEA sentence to read the value from.
+/// \param field_index The index of the field in the NMEA sentence.
+void set_float(nmea::field<float>& field, const nmea::sentence& sentence, uint8_t field_index);
+/// \brief Parses an enumeration field from an NMEA sentence.
+/// \tparam enum_type The object type of the enumeration to parse.
+/// \param field The NMEA field to parse the value into.
+/// \param sentence The NMEA sentence to read the value from.
+/// \param field_index The index of the field in the NMEA sentence.
+template <class enum_type>
+void set_enum(nmea::field<enum_type>& field, const nmea::sentence& sentence, uint8_t field_index)
+{
+    // Get field string.
+    std::string field_string = sentence.get_field(field_index);
+
+    // Check if field exists.
+    if(!field_string.empty())
+    {
+        // Set field with enum value.
+        field.set(static_cast<enum_type>(std::stoi(field_string)));
+    }
+}
+
+// EXTENDED
+/// \brief Parses UTC time of day from an NMEA sentence.
+/// \param field The NMEA field to parse the value into.
+/// \param sentence The NMEA sentence to read the value from.
+/// \param field_index The index of the field in the NMEA sentence.
+void set_utc(nmea::field<double>& field, const nmea::sentence& sentence, uint8_t field_index);
+/// \brief Parses latitude from an NMEA sentence.
+/// \param field The NMEA field to parse the value into.
+/// \param sentence The NMEA sentence to read the value from.
+/// \param field_index The index of the field in the NMEA sentence.
+void set_latitude(nmea::field<double>& field, const nmea::sentence& sentence, uint8_t field_index);
+/// \brief Parses longitude from an NMEA sentence.
+/// \param field The NMEA field to parse the value into.
+/// \param sentence The NMEA sentence to read the value from.
+/// \param field_index The index of the field in the NMEA sentence.
+void set_longitude(nmea::field<double>& field, const nmea::sentence& sentence, uint8_t field_index);
 
 }}
 
